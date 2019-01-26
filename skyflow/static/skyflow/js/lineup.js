9d5526465362628d00ea9249a2f232cddc8dfaab
wooilkim <wooilkim@korea.ac.kr>
2019-01-26 19:48:02 +0900

    라인업 추가

---
+++ skyflow/static/skyflow/js/lineup.js
@@ -0,0 +1,43 @@
+d3.csv("/static/skyflow/data/processed/NBA.csv").then(function (data) {
+
+
+// arr from before
+    const arr = [];
+    // const cats = ['c1', 'c2', 'c3'];
+    // for (let i = 0; i < 100; ++i) {
+    //     arr.push({
+    //         a: Math.random() * 10,
+    //         d: 'Row ' + i,
+    //         cat: cats[Math.floor(Math.random() * 3)],
+    //         cat2: cats[Math.floor(Math.random() * 3)]
+    //     })
+    // }
+
+    const builder = LineUpJS.builder(data);
+    var g = d3.scaleOrdinal(d3.schemeCategory10);
+// manually define columns
+    columns = data.columns.slice(6, 15);
+    columns.forEach(function (c, c_i) {
+        builder.column(LineUpJS.buildNumberColumn(c, d3.extent(data.map(x => x[c]))).color(g(c_i)))
+    });
+    // builder
+    //     .column(LineUpJS.buildStringColumn('d').label('Label').width(100))
+    //     .column(LineUpJS.buildCategoricalColumn('cat', cats).color('green'))
+    //     .column(LineUpJS.buildCategoricalColumn('cat2', cats).color('blue'))
+    //     .column(LineUpJS.buildNumberColumn('a', [0, 10]).color('blue'));
+
+// and two rankings
+//     const ranking = LineUpJS.buildRanking()
+//         .supportTypes()
+//         .allColumns() // add all columns
+//         .impose('a+cat', 'a', 'cat2') // create composite column
+//         .groupBy('cat')
+//         .sortBy('a', 'desc');
+//
+//
+//     builder
+//         .defaultRanking()
+//         .ranking(ranking);
+
+    const lineup = builder.build(document.body);
+});
